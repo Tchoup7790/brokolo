058ac5d51ed8d05bc6bb69b78f99a0409c5bbd90
Baptiste <113362872+Tchoup7790@users.noreply.github.com>
2024-11-19 12:03:35 +0100

    Make Header and homepage

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">
+        <small>01</small>
+        Home
+      </RouterLink>
+      <RouterLink to="/work">
+        <small>02</small>
+        Work
+      </RouterLink>
+      <RouterLink to="/contact">
+        <small>03</small>
+        Contact
+      </RouterLink>
+    </nav>
+
+    <div>
+      <p class="city">
+        Nantes [FR]
+      </p>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  z-index: 2;
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: fit-content;
+  width: 100vw;
+  padding: 1rem;
+  display: none;
+  justify-content: space-between;
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  font-size: 1rem;
+}
+
+nav a.router-link-exact-active {
+  cursor: default;
+  color: var(--color-text);
+  overflow: hidden;
+}
+
+nav a.router-link-exact-active::before {
+  background-color: var(--colo);
+}
+
+.switch {
+  color: var(--color-text-soft);
+  cursor: pointer;
+  transition: 0.5s opacity;
+}
+
+.switch:hover {
+  color: var(--color-text);
+}
+
+.city {
+  color: var(--color-text-soft);
+}
+
+@media (min-width: 425px) {
+  header {
+    display: inline-flex;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,61 @@
+<template>
+  <Transition appear>
+    <main>
+      <h1>Hello, I'M <span>Baptiste</span></h1>
+      <h1>A <span>Developer</span> and</h1>
+      <h1><span>Student</span> who</h1>
+      <h1>Create Website</h1>
+    </main>
+  </Transition>
+</template>
+
+<style scoped>
+main {
+  text-align: center
+}
+
+h1 {
+  position: relative;
+}
+
+span {
+  position: relative;
+  border-radius: 7px;
+  overflow: hidden;
+}
+
+span::before {
+  position: absolute;
+  content: " ";
+  width: 100%;
+  height: 1.5rem;
+  left: 0;
+  top: 0;
+  z-index: -1;
+  transform: scaleX(1);
+  transform-origin: left bottom;
+  background-color: var(--color-primary-soft);
+  transition: transform 0.9s 0.3s cubic-bezier(0.96, 0, 0.5, 0.91);
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.6s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter-from span::before,
+.v-leave-to span::before {
+  transform: scaleX(0);
+}
+
+@media (min-width: 425px) {
+  span::before {
+    height: 5.5rem;
+  }
+}
+</style>
